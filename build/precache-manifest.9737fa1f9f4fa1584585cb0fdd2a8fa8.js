ad546841479367bf8fd11dafa4095b0ad61d6dca
heliumface770 <heliumface770@gmail.com>
2020-03-09 16:58:39 -0600

    heroku deployment

---
+++ build/precache-manifest.9737fa1f9f4fa1584585cb0fdd2a8fa8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39ea107923af4a6bb61b6e04f83a22f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a56e147f42e0239bc14f",
+    "url": "/static/css/main.be91ac31.chunk.css"
+  },
+  {
+    "revision": "744ef6255e1b040aa4b3",
+    "url": "/static/js/2.69a8afa2.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.69a8afa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56e147f42e0239bc14f",
+    "url": "/static/js/main.04aaab3b.chunk.js"
+  },
+  {
+    "revision": "2ab5528f29807ec3af41",
+    "url": "/static/js/runtime-main.8b054bcd.js"
+  },
+  {
+    "revision": "0992a2b04558e2695894c4f643778467",
+    "url": "/static/media/Resume.0992a2b0.pdf"
+  },
+  {
+    "revision": "a95ec222e7df12475f4e063d02139b0c",
+    "url": "/static/media/Resume.a95ec222.png"
+  },
+  {
+    "revision": "ef547705318b42a6a253e2bf09185117",
+    "url": "/static/media/bonez-beatz.ef547705.JPG"
+  },
+  {
+    "revision": "97b4228bfec1746d13951ac1452ed0d2",
+    "url": "/static/media/flintz-skins.97b4228b.png"
+  },
+  {
+    "revision": "b3b4ee1ca35da3774b975273856f62eb",
+    "url": "/static/media/github.b3b4ee1c.png"
+  },
+  {
+    "revision": "6c5e0efad626ab056239a4d1d2c1d108",
+    "url": "/static/media/nasa-api.6c5e0efa.JPG"
+  },
+  {
+    "revision": "88004197555b0d6e1dd705226b78449e",
+    "url": "/static/media/note-taker.88004197.png"
+  },
+  {
+    "revision": "e77f09ae8ad931e92765c9a0c769ca7a",
+    "url": "/static/media/password.e77f09ae.JPG"
+  },
+  {
+    "revision": "9fe94c1ca4546fdd97a9c29f794ba487",
+    "url": "/static/media/weather-app.9fe94c1c.JPG"
+  }
+]);
